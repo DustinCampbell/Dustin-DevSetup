17e9aa45561605328f2cf53c7a4cecd48a7c6382
Dustin Campbell <dustin@teamcampbell.org>
2024-01-29 10:37:43 -0800

    Add details for installing programmer fonts and setting up terminal

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# DevSetup
+# Dustin's Developer Setup
+
+## Programmer Fonts
+
+- Install [Nerd Fonts](https://www.nerdfonts.com/font-downloads)
+- For editors, such as VS, VS Code, or Rider, use 'FiraMono Nerd Font'
+- For command-line, use 'FiraCode Nerd Font'
+
+## Command Prompt
+
+I use Windows Terminal as my terminal interface. A better theme can be acquired at [Windows Terminal Themes](https://windowsterminalthemes.dev/). I particularly like "Dark Pastel".
+
+- Install the latest official release version of [PowerShell 7](https://github.com/PowerShell/PowerShell/releases/)
+- Install [Starship](https://starship.rs/)
+  - From the Windows command-line, use `winget` to install: `winget install -id Starship.Starship`
+- Copy `files\starship.toml` to `$HOME\.starship\`
+- Copy contents of `files\profile.ps1` into the PowerShell profile at `$PROFILE`
---
+++ files/profile.ps1
@@ -0,0 +1,88 @@
+function Initialize-VS {
+    param(
+        [Parameter(Position=0, Mandatory=$false)]
+        [Switch]$chooseVS
+    )
+
+    function Get-VS-Install-Label($vsInstall) {
+        $channelId = $vsInstall.installedChannelId
+        $lastDotIndex = $channelId.LastIndexOf(".")
+        $channelName = $channelId.Substring($lastDotIndex + 1);
+
+        return "$($vsInstall.displayName) ($($vsInstall.installationVersion) - $($channelName))"
+    }
+
+    $vswhere = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
+
+    if ($chooseVS) {
+        # Launch vswhere.exe to retrieve a list of installed Visual Studio instances
+        $vsInstallsJson = &$vswhere -prerelease -format json
+        $vsInstalls = $vsInstallsJson | ConvertFrom-Json
+
+        # Display a menu of Visual Studio instances to the user
+        Write-Host ""
+
+        $vsInstallLabels = @()
+        $index = 1
+
+        foreach ($vsInstall in $vsInstalls) {
+            $vsInstallLabel = Get-VS-Install-Label $vsInstall
+            $vsInstallLabels += $vsInstallLabel
+            Write-Host "    $($index) - $($vsInstallLabel)"
+            $index += 1
+        }
+
+        Write-Host ""
+        $choice = [int](Read-Host "Choose a Visual Studio version to launch")
+
+        $vsInstall = $vsInstalls[$choice - 1]
+    }
+    else {
+        # Launch vswhere.exe to retrieve the newest, last installed Visual Studio instance
+        $vsInstallJson = &$vswhere -prerelease -latest -format json
+        $vsInstalls = $vsInstallJson | ConvertFrom-Json
+        $vsInstall = $vsInstalls[0]
+    }
+
+    Write-Host ""
+    Write-Host "Initializing dev prompt for $(Get-VS-Install-Label $vsInstall)..."
+    Write-Host ""
+
+    $vsPath = $vsInstall.installationPath
+
+    Import-Module (Get-ChildItem $vsPath -Recurse -File -Filter Microsoft.VisualStudio.DevShell.dll).FullName
+    Enter-VsDevShell -VsInstallPath $vsPath -SkipAutomaticLocation -DevCmdArguments '-arch=x64'
+
+    Write-Host ""
+
+    Set-Alias VS devenv.exe -Scope Global
+}
+
+function Set-LocationProjects() {
+    $envName = "PROJECTS_ROOT"
+    $projectsPath = [Environment]::GetEnvironmentVariable($envName);
+
+    if ($projectsPath -eq $null) {
+        Write-Error "Please define the '${envName}' environment variable to set that location."
+        return $false
+    }
+
+    if (-not (Test-Path $projectsPath)) {
+        Write-Error "'${envName}' environment variable does not exist: {$projectsPath}."
+        return $false
+    }
+
+    Set-Location -Path $projectsPath
+
+    return $true
+}
+
+Set-Alias init-vs Initialize-VS
+Set-Alias projects Set-LocationProjects
+Set-Alias open Explorer
+
+Set-LocationProjects | Out-Null
+
+# Initialize Starship Prompt
+$ENV:STARSHIP_CONFIG = "$HOME\.starship\starship.toml"
+Invoke-Expression (&starship init powershell)
